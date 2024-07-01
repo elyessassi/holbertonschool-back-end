7c0538aad1d652d9bda77bbca0812edc9aa56f65
elyessassi <mjoxen@gmail.com>
2024-07-01 14:37:20 +0100

    fonction that shows user name and tasks done

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,7 +3,7 @@ import requests
 import sys
 
 
-def todo(int(sys.argv[1])):
+def todo():
     """fonction that shows user name and tasks done"""
     id_value = int(sys.argv[1])
     responsetodo = requests.get("https://jsonplaceholder.typicode.com/todos")
